da24d0956acbd2fdb42882f4361b9edaa6249ba8
Mateen <mateenqazi125@gmail.com>
2023-08-04 18:35:20 +0500

    implementation of unary

--- client/main.go
+++ client/main.go
@@ -3,18 +3,25 @@ package main
 import (
 	"log"
 
+	"github.com/mateenqazi/grpc-with-go/proto"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
 const (
-	port = ":8080 "
+	port = ":8080"
 )
 
 func main() {
 	conn, err := grpc.Dial("localhost"+port, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
 	if err != nil {
 		log.Fatalf("Did not connect: %v", err)
 	}
 	defer conn.Close()
+
+	client := proto.NewGreetServiceClient(conn)
+
+	CallSayHello(client)
+
 }
